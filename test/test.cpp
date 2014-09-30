8ab34149d01a68f9374cb4a99af248c12267646f
Albus Smith <albusmath@gmail.com>
2014-09-30 15:10:22 +0800

    Tue Sep 30 15:10:22 CST 2014

--- test/test.cpp
+++ test/test.cpp
@@ -1,25 +1,47 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+
+extern "C" {
+#include <GL/glut.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
+}
 
 using namespace std;
 
 int
-main (void)
+main (int argc, char *argv[])
 {
-	int i, j;
-	gsl_matrix * m = gsl_matrix_alloc (10, 3);
+	gsl_matrix * m = gsl_matrix_alloc(1000, 1000);
 
-	for (i = 0; i < 10; i++)
-		for (j = 0; j < 3; j++)
-			gsl_matrix_set (m, i, j, 0.23 + 100*i + j);
+	gsl_matrix * a = gsl_matrix_alloc(1000, 1000);
+	gsl_matrix * b = gsl_matrix_alloc(1000, 1000);
 
-	for (i = 0; i < 10; i++)  /* OUT OF RANGE ERROR */
-		for (j = 0; j < 3; j++)
-			printf ("m(%d,%d) = %g\n", i, j,
-					gsl_matrix_get (m, i, j));
+	for (int i = 0;i < (int)a->size1;i++) {
+		for (int j = 0;j < (int)a->size2;j++) {
+			gsl_matrix_set(a, i, j, (rand() % 10000)/100.);
+			gsl_matrix_set(b, i, j, (rand() % 10000)/100.);
+		}
+	}
+	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 
+					1., a, b, 
+					0., m);
+
+	//gsl_matrix_fprintf(stdout, m, "%g");
 
 	gsl_matrix_free (m);
+	gsl_matrix_free (b);
+	gsl_matrix_free (a);
 
+	glutInit(&argc, argv);   
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);   
+	glutInitWindowSize(500, 500);   
+	glutInitWindowPosition(100, 100);   
+	glutCreateWindow(argv[0]);   
+//	glutKeyboardFunc(key_press_callback);   
+	//glutDisplayFunc(display);   
+	//glutReshapeFunc(reshape);   
+	glutMainLoop();   
 	return 0;
 }
